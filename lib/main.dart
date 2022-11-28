5e95ffb993bf4dd639450b41925687f1d21e5e0a
WorkStation <afrasyab591@gmail.com>
2022-11-28 22:38:45 +0500

    Monthly Framework Screen

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,6 @@ import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:we_skool_app/providers/multi_providers.dart';
 import 'package:we_skool_app/routes/routes.dart';
-import 'package:we_skool_app/screens/Blog%20Articles/blog_article_screen.dart';
-import 'package:we_skool_app/screens/Misclaneous/misc.dart';
-import 'package:we_skool_app/screens/Read/read_screen.dart';
-import 'package:we_skool_app/screens/WeSpecials/wespecial.dart';
-import 'package:we_skool_app/screens/contact_us/contact_us.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
